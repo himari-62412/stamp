9264eaa2b4bd75d97c3e166997ab85a9efcca8c3
Himari Nakamura <himari.nakamura@gmail.com>
2024-01-30 20:25:11 +0900

    新しいコレクションビューを作った

---
+++ メモ/SeasonCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SeasonCollectionViewCell.swift
+//  Memo
+//
+//  Created by 中村日鞠 on 2024/01/30.
+//
+
+import UIKit
+
+class SeasonCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
